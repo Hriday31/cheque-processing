4f5fe2dcd2492e4c63aa534c17385df9ee8a650f
HRA <Hriday Aggarwal>
2022-09-29 22:29:45 +0530

    New changes

--- 1.py
+++ 1.py
@@ -7,19 +7,19 @@ import csv
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning)
 
-image = cv2.imread('test.png')  # read cheque image
+image = cv2.imread('test.jpeg')  # read cheque image
 image1 = cv2.imread('sign.jpeg')
 
 # resizing to prevent any mismatch during scanning of cheque
-img = cv2.resize(image, (804, 370))
+img = cv2.resize(image, (1170, 538))
 
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # convert image to grayscal
 signm1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
 
-sign = img[170:255, 570:804]  # image cropping to extract details
-name = img[255:280, 570:800]
-acc = img[180:210, 75:300]
-amt = img[135:165, 620:775]
+sign = img[290:378, 900:1128]  # image cropping to extract details
+name = img[250:290, 690:1130]
+acc = img[265:298,128:395]
+amt = img[195:240, 890:1110]
 
 # display images
 cv2.imshow('Orignal Cheque',image)
@@ -30,11 +30,11 @@ cv2.imshow('Amount',amt)
 
 
 def imgtotext(p):  # extracting text from collected information
-    path_to_tesseract = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-    pytesseract.tesseract_cmd = path_to_tesseract
+    # path_to_tesseract = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+    # pytesseract.tesseract_cmd = path_to_tesseract
     text = pytesseract.image_to_string(
         p, lang='eng', config='--psm 6 tessedit_char_whitelist=0123456789')
-    return (text[0:-1])
+    return (text[0:-2])
 
 
 print("Amount:", imgtotext(amt))
@@ -67,14 +67,16 @@ sign3 = remove_white_space(sign)
 cheque_image = cv2.resize(sign3, (100, 100))
 original_image = cv2.resize(sign2, (100, 100))
 
-cv2.imshow('Recorded signature', original_image)
-cv2.imshow('Sign on cheque', cheque_image)
+cv2.imshow('Recorded signature', sign2)
+# cv2.imshow('Sign on cheque', cheque_image)
 
 
-print("Error %-", ssim(cheque_image, original_image)*100)
+print("Match %-", ssim(cheque_image, original_image)*100)
 
-if ssim(cheque_image, original_image) <= 0.2:
+if ssim(cheque_image, original_image) >= 0.8:
     print("Signatures Matched - Transaction Approved")
+elif ssim(cheque_image, original_image) >= 0.4 :
+    print("Slight discrepencies-Manual approval suggested.")
 else:
     print("Signatures do not Match - Transaction Failed")
 f = open('chequeprocessing.csv', 'a', newline='')
